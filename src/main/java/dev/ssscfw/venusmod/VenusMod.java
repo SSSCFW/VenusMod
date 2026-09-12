0ab5b673fa9a8a66b83cef0474cc16b6a0707492
SSSCFW <85436308+SSSCFW@users.noreply.github.com>
2026-09-12 18:24:28 +0900

    feat: register Venus combat filtering

--- src/main/java/dev/ssscfw/venusmod/VenusMod.java
+++ src/main/java/dev/ssscfw/venusmod/VenusMod.java
@@ -1,10 +1,12 @@
 package dev.ssscfw.venusmod;
 
+import dev.ssscfw.venusmod.event.VenusCombatEvents;
 import dev.ssscfw.venusmod.registry.ModEntities;
 import dev.ssscfw.venusmod.registry.ModItems;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.common.NeoForge;
 
 @Mod(VenusMod.MOD_ID)
 public final class VenusMod {
@@ -16,5 +18,7 @@ public final class VenusMod {
 
         modBus.addListener(ModEntities::registerAttributes);
         modBus.addListener(ModItems::addCreativeTabContents);
+
+        NeoForge.EVENT_BUS.addListener(VenusCombatEvents::onLivingIncomingDamage);
     }
 }
