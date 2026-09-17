6e31530586c4082b30983f1ea0e8ccb3de87362f
SSSCFW <85436308+SSSCFW@users.noreply.github.com>
2026-09-17 11:49:03 +0900

    feat: register King Treasury menu type

---
+++ src/main/java/dev/ssscfw/venusmod/registry/ModMenus.java
@@ -0,0 +1,20 @@
+package dev.ssscfw.venusmod.registry;
+
+import dev.ssscfw.venusmod.VenusMod;
+import dev.ssscfw.venusmod.treasury.KingsTreasuryMenu;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.world.flag.FeatureFlags;
+import net.minecraft.world.inventory.MenuType;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public final class ModMenus {
+    public static final DeferredRegister<MenuType<?>> MENUS =
+            DeferredRegister.create(Registries.MENU, VenusMod.MOD_ID);
+
+    public static final DeferredHolder<MenuType<?>, MenuType<KingsTreasuryMenu>> KING_TREASURY =
+            MENUS.register("king_treasury",
+                    () -> new MenuType<>(KingsTreasuryMenu::new, FeatureFlags.DEFAULT_FLAGS));
+
+    private ModMenus() {}
+}
